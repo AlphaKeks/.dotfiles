339e0ddeb72daef0f48391a05ea04c79e214dd44
AlphaKeks <alphakeks@dawn.sh>
2023-05-31 14:20:13 +0200

    feat: wezterm config

---
+++ configs/terminals/wezterm/wezterm.lua
@@ -0,0 +1,59 @@
+-- https://GitHub.com/AlphaKeks/.dotfiles
+
+local term = require("wezterm")
+
+return {
+  default_prog = { "zsh" },
+  color_scheme = "Catppuccin Mocha",
+  -- font = term.font_with_fallback {
+  --   "Iosevka Nerd Font",
+  --   -- "Liga SFMono Nerd Font",
+  --   "JetBrains Mono",
+  --   "Fira Code Nerd Font",
+  -- },
+  font_size = 18,
+  line_height = 1,
+  use_fancy_tab_bar = false,
+  hide_tab_bar_if_only_one_tab = true,
+  tab_bar_at_bottom = false,
+  -- window_background_opacity = 0.80,
+  window_background_opacity = 1,
+  -- default_cursor_style = "BlinkingBlock",
+  cursor_blink_rate = 727,
+  cursor_blink_ease_in = "Linear",
+  cursor_blink_ease_out = "Linear",
+  animation_fps = 240,
+  scrollback_lines = 1000000,
+  audible_bell = "Disabled",
+  window_padding = {
+    left = 4,
+    right = 4,
+    top = 4,
+    bottom = 0
+  },
+  keys = {
+    {
+      key = "f",
+      mods = "CTRL|SHIFT",
+      action = term.action.SendString("source ~/.local/bin/scripts/gtp.sh\r"),
+    },
+    {
+      key = "^",
+      mods = "CTRL",
+      action = term.action.SendKey({
+        key = "^",
+        mods = "CTRL",
+      }),
+    },
+    {
+      key = "Backspace",
+      mods = "CTRL",
+      action = term.action.SendKey({
+        key = "w",
+        mods = "CTRL",
+      }),
+    },
+  },
+}
+
+-- vim: et ts=2 sw=2 sts=2 ai si ft=lua
