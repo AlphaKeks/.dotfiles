c493b561f4ad490bc9c3642fbd04812612a88353
AlphaKeks <alphakeks@dawn.sh>
2023-06-27 23:56:08 +0200

    switch back to oil

---
+++ configs/editors/nvim/after/plugin/oil.lua
@@ -0,0 +1,52 @@
+-- https://GitHub.com/AlphaKeks/.dotfiles
+
+local oil_installed, oil = pcall(require, "oil")
+
+if not oil_installed then
+	return
+end
+
+oil.setup({
+	columns = {
+		"icon",
+		"mtime",
+	},
+
+	buf_options = {
+		buflisted = false,
+		bufhidden = "hide",
+	},
+
+	win_options = {
+		wrap = true,
+		cursorcolumn = true,
+	},
+
+	default_file_explorer = true,
+
+	keymaps = {
+		["g?"] = "actions.show_help",
+		["<CR>"] = "actions.select",
+		["<C-v>"] = "actions.select_vsplit",
+		["<C-t>"] = "actions.select_tab",
+		["<C-p>"] = "actions.preview",
+		["<ESC>"] = "actions.close",
+		["<C-l>"] = "actions.refresh",
+		["<BS>"] = "actions.parent",
+		["g."] = "actions.toggle_hidden",
+	},
+
+	use_default_keymaps = true,
+
+	view_options = {
+		show_hidden = true,
+	},
+
+	preview = {
+		win_options = {
+			winblend = 10,
+		},
+	},
+})
+
+nn("<Leader>e", oil.open)
--- configs/editors/nvim/lua/alphakeks/plugins.lua
+++ configs/editors/nvim/lua/alphakeks/plugins.lua
@@ -40,7 +40,7 @@ lazy.setup({
 		"hrsh7th/cmp-path",
 		"hrsh7th/cmp-nvim-lsp",
 		"L3MON4D3/LuaSnip",
-		"tamago324/lir.nvim",
+		"stevearc/oil.nvim",
 	},
 
 	concurrency = 69,
