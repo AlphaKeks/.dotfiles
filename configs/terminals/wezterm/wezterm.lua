7ff4e3e4db06ffac1b800a82e6cf3cd9e0038723
AlphaKeks <alphakeks@dawn.sh>
2023-07-03 19:40:04 +0200

    wezterm

--- configs/terminals/wezterm/wezterm.lua
+++ configs/terminals/wezterm/wezterm.lua
@@ -5,29 +5,14 @@ local term = require("wezterm")
 return {
 	default_prog = { "zsh" },
 	color_scheme = "Catppuccin Mocha",
-	colors = {
-		background = "#181825",
-	},
-	-- font = term.font({ family = "JetBrains Mono" }),
-	font = term.font({ family = "MonoLisa-Regular" }),
-	font_size = 16,
-	-- font_rules = {
-	-- 	{
-	-- 		intensity = "Normal",
-	-- 		italic = true,
-	-- 		font = term.font({
-	-- 			family = "JetBrains Mono Italic",
-	-- 			style = "Italic",
-	-- 		}),
-	-- 	},
-	-- },
+	colors = { background = "#181825" },
+	font = term.font({ family = "JetBrains Mono" }),
+	font_size = 12,
 	line_height = 1,
 	use_fancy_tab_bar = false,
 	hide_tab_bar_if_only_one_tab = true,
 	tab_bar_at_bottom = false,
-	-- window_background_opacity = 0.80,
 	window_background_opacity = 1,
-	-- default_cursor_style = "BlinkingBlock",
 	cursor_blink_rate = 727,
 	cursor_blink_ease_in = "Linear",
 	cursor_blink_ease_out = "Linear",
